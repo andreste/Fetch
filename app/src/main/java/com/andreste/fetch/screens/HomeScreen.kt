851ca3298d0ddc7bbbbc2c08c1d5cabb0124a88d
Andres Trevino <andreste24@gmail.com>
2024-11-22 11:39:38 -0500

    Show items

--- app/src/main/java/com/andreste/fetch/screens/HomeScreen.kt
+++ app/src/main/java/com/andreste/fetch/screens/HomeScreen.kt
@@ -1,14 +1,31 @@
 package com.andreste.fetch.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TextField
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.andreste.fetch.models.HiringItem
 import com.andreste.fetch.viewmodels.HiringViewModel
+import com.andreste.fetch.viewmodels.ViewState
 
 @Composable
 fun HomeScreen(navController: NavController, viewModel: HiringViewModel) {
@@ -16,6 +33,98 @@ fun HomeScreen(navController: NavController, viewModel: HiringViewModel) {
         viewModel.start(navController)
         viewModel.getItems()
     }
-    TextField(value = "hello", onValueChange = {})
 
+    viewModel.state.collectAsState().value.let { state ->
+        when (state) {
+            is ViewState.Content -> {
+                if (state.list.isEmpty()) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "No stocks found",
+                            modifier = Modifier.padding(16.dp),
+                            textAlign = TextAlign.Center
+                        )
+                    }
+
+                } else {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .fillMaxWidth()
+                    ) {
+                        items(state.list) {
+                            NotificationRow(it)
+                        }
+                    }
+                }
+            }
+
+            is ViewState.Error -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Error ${state.message}",
+                        modifier = Modifier.padding(16.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            ViewState.Loading -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(48.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun NotificationRow(item: HiringItem) {
+
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(0.33f)
+        ) {
+            Text(text = item.id.toString())
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(0.33f)
+        ) {
+            Text(text = item.name ?: "")
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(0.33f)
+        ) {
+            Text(text = item.listId.toString())
+        }
+
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(Color.Gray)
+    )
 }
